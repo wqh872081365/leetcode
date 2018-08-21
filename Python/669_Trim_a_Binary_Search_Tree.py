ec8f144fdb140aa08d1ed2d6142947d20a6e30d6
wangqihui <wangqihui0324@gmail.com>
2018-08-21 17:42:14 +0800

    1

---
+++ Python/669_Trim_a_Binary_Search_Tree.py
@@ -0,0 +1,67 @@
+"""
+Given a binary search tree and the lowest and highest boundaries as L and R, trim the tree so that all its elements lies in [L, R] (R >= L). You might need to change the root of the tree, so the result should return the new root of the trimmed binary search tree.
+
+Example 1:
+Input:
+    1
+   / \
+  0   2
+
+  L = 1
+  R = 2
+
+Output:
+    1
+      \
+       2
+Example 2:
+Input:
+    3
+   / \
+  0   4
+   \
+    2
+   /
+  1
+
+  L = 1
+  R = 3
+
+Output:
+      3
+     /
+   2
+  /
+ 1
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+
+        def trim(node):
+            if node:
+                if node.val < L:
+                    node = trim(node.right)
+                elif node.val > R:
+                    node = trim(node.left)
+                else:
+                    node.left = trim(node.left)
+                    node.right = trim(node.right)
+            return node
+
+        node = trim(root)
+        return node
